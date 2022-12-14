b2fbdf5f26295a5a18054a47dfb686f8d9829dd2
Jhunyingan <jhunyingan1@gmail.com>
2022-12-14 07:09:13 -0800

    task0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,8 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-void print_times_tables(int n);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
