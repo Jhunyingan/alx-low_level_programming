56d4a975cc863bf8663f236e2d8160c43095529a
Jhunyingan <jhunyingan1@gmail.com>
2022-12-13 21:30:27 -0800

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * add - Entry point for the addition
+  * @a: one value to be added together
+  * @b: second value to be added
+  * Return: Always 0 (Success)
+  */
+
+int add(int a, int b)
+{
+	_putchar('a');
+	_putchar('+');
+	_putchar('b');
+
+	return (a + b);
+}
