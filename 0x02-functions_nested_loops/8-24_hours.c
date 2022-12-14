b8020ed2951eae8894336fa87d602b5ac789a01e
Jhunyingan <jhunyingan1@gmail.com>
2022-12-13 20:50:05 -0800

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,9 +11,9 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-	for (h = 0; h <= 24; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (m = 0; m <= 60; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
