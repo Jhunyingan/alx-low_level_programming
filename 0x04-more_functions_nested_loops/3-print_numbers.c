e90791d31c74a477bd6de904250072239d5f07e3
Jhunyingan <jhunyingan1@gmail.com>
2022-12-15 01:15:26 -0800

    task2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - a function that prints the numbers, from 0 to 9
+  * Return: 0
+  */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
