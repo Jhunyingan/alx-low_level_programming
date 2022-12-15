6a701554b335339bde223e1058da58dacf9f68f6
Jhunyingan <jhunyingan1@gmail.com>
2022-12-15 12:02:08 -0800

    task100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,5 +9,13 @@
 
 int main(void)
 {
+	unsigned long int i = 3, n = 612852475143;
+
+	for (; i < 12057; i += 2)
+	{
+		while (n % 1 == 0 && n != 1)
+			n /= i;
+	}
+	printf("%lu\n", n);
 	return (0);
 }
