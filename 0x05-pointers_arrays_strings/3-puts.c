374ce8391475cc8ab0f1e87af5b1983f9e727e66
Jhunyingan <jhunyingan1@gmail.com>
2022-12-19 09:56:02 -0800

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _puts - a function that prints a string, followed by a new line
+  * @str: pointer value
+  * Return: Always 0 (Success)
+  */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
