8c032ba2e3984dd400c16df3b03818eea098cce0
Jhunyingan <jhunyingan1@gmail.com>
2022-12-19 08:13:13 -0800

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * reset_to_98 - update pointer value to 98
+  * @n: parameter to be given
+  * Return: Always 0 (Success)
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
