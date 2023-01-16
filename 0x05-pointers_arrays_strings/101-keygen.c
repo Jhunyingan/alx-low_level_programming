0d2d2054d37da5f6939c6d37dd71b75dd381501c
Jhunyingan <jhunyingan1@gmail.com>
2023-01-16 05:08:17 -0800

    task2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
-  * main - generates random valid passwords
-  * Return: 0 if success
-  */
+#define MIN_PASS_LENGTH 8
+#define MAX_PASS_LENGTH 16
 
 int main(void)
 {
-	int pass, sum;
-
 	srand(time(NULL));
-	sum = 0;
-	while (sum <= 2645)
+
+	int pass_length = MIN_PASS_LENGTH + (rand() % (MAX_PASS_LENGTH - MIN_PASS_LENGTH + 1));
+	char password[pass_length + 1];
+
+	int i= 0;
+	while (i < pass_length)
 	{
-		pass = (rand() % 128);
-		sum += pass;
-		printf("%c", pass);
+		char c = (rand() % 94) + 33;
+		if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '!' || c == '@' || c == '#' || c == '$' || c == '%' || c == '^' || c == '&' || c == '*' || c == '(' || c == ')' || c == '-' || c == '_' || c == '+' || c == '=' || c == '{' || c == '}' || c == '[' || c == ']' || c == '|' || c == '\' || c == ':' || c == ';' || c == '"' || c == '<' || c == '>' || c == '?' || c == ',' || c == '.' || c == '/' || c == '~')
+		{
+			password[i] = c;
+			i++;
+		}
 	}
-
-	printf("%c", 2772 - sum);
-
+	password[pass_length] = '\0';
+	printf("Generated password: %s\n", password);
 	return (0);
 }
